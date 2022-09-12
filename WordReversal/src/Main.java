b11ab5b8e9e3337c1880736f498bbaec14b9fc2a
furr89 <46800810+furr89@users.noreply.github.com>
2022-09-12 07:42:01 -0400

    refactor and clean up code

--- WordReversal/src/Main.java
+++ WordReversal/src/Main.java
@@ -1,9 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,16 +9,18 @@ public class Main {
         System.out.println("Enter phrase: ");
         String phrase = scan.nextLine();
 
-        int endOfPhrase = phrase.length() - 1;
+        int phraseLength = phrase.length() - 1;
         char lastChar = '\0';
+        boolean isDigit = Character.isDigit(phrase.charAt(phraseLength));
+        boolean isLetter = Character.isLetter(phrase.charAt(phraseLength));
+        boolean isSpaces = Character.isWhitespace(phrase.charAt(phraseLength));
 
         //check if the last char is a special character
-        if (!Character.isDigit(phrase.charAt(endOfPhrase)) && !Character.isLetter(phrase.charAt(endOfPhrase))
-                && !Character.isWhitespace(phrase.charAt(endOfPhrase))) {
+        if (!isDigit && !isLetter && !isSpaces) {
 
-            //get last char and remove it if its a special char
-            lastChar = phrase.charAt(phrase.length() - 1);
-            phrase = phrase.substring(0, phrase.length() - 1);
+            //get last char and remove it if it's a special char
+            lastChar = phrase.charAt(phraseLength);
+            phrase = phrase.substring(0, phraseLength);
         }
 
         //store phrase in array
